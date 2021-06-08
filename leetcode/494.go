d660f6c5fc52d45684248e5e16323a4cf94dc6c3
lixuancn <lixuan868686@163.com>
2021-06-08 10:26:52 +0800

    494

--- leetcode/494.go
+++ leetcode/494.go
@@ -4,31 +4,62 @@ import (
 )
 
 func main() {
-	r := findTargetSumWays([]int{1,1,1,1,1}, 3)
+	r := findTargetSumWays([]int{1000}, -1000)
 	fmt.Println(r)
 }
 
-//动态规划
+//动态规划优化版
+//每次循环只和上一行有关，所以去掉数组第一维。
 func findTargetSumWays(nums []int, target int) int {
 	sum := 0
-	for _, num :=range nums{
+	for _, num := range nums{
 		sum += num
 	}
 	diff := sum - target
-	if diff < 0 || diff % 2 == 1{
+	if diff < 0 || diff % 2 != 0{
 		return 0
 	}
-	n, neg := len(nums), diff / 2
-	dp := make([][]int, n+1)
-	for i := range dp{
-		dp[i] = make([]int, neg+1)
+	neg := diff / 2
+	dp := make([]int, neg+1)
+	dp[0] = 1
+	for i := range nums{
+		for j:=neg; j>=nums[i]; j--{
+			dp[j] += dp[j-nums[i]]
+		}
 	}
+	return dp[neg]
+}
+
+//动态规划
+//元素和为sum，加-号的元素和是neg，加+号的元素和就是sum-neg。
+//target = (sum - neg) - neg = sum - 2*neg 则 neg = (sum-target)/2
+//nums中都是非负整数，所以neg也是非负整数，则sum-target是非负偶数。
+//问题转化为：在nums中选取若干元素，使元素和为neg，求方案数。可用动态规划
+//定义dp[i][j]，表示nums的前i个元素中选择若干元素，元素和为j的组合数。
+//最终所需结果为dp[n][neg]
+//边界，0个元素时，和为0，方案数为1。dp[0][0] = 1
+//1<=i<=n时，遍历0<=j<=neg计算dp[i][j]：
+//	如果j<num，则不能选num，dp[i][j] = dp[i-1][j]
+//	如果j>num，如果不选num，dp[i][j] = dp[i-1][j]，如果选，dp[i][j] = dp[i-1][j]+dp[i-1][j-num]
+func findTargetSumWays_dp(nums []int, target int) int {
+	sum, n := 0, len(nums)
+	for _, num := range nums{
+		sum += num
+	}
+	diff := sum - target
+	if diff < 0 || diff % 2 != 0{
+		return 0
+	}
+	neg := diff / 2
+	dp := make([][]int, n+1)
+	dp[0] = make([]int, neg+1)
 	dp[0][0] = 1
-	for i, num := range nums{
+	for i := range nums{
+		dp[i+1] = make([]int, neg+1)
 		for j:=0; j<=neg; j++{
 			dp[i+1][j] = dp[i][j]
-			if j >= num{
-				dp[i+1][j] += dp[i][j-num]
+			if j >= nums[i]{
+				dp[i+1][j] += dp[i][j-nums[i]]
 			}
 		}
 	}
