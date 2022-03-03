469cd5fdadb8c915979908affe91c087aa418132
lixuancn <lixuan868686@163.com>
2022-03-03 14:14:40 +0800

    27 双指针+快慢指针+暴力

--- leetcode/27.go
+++ leetcode/27.go
@@ -2,32 +2,38 @@ package main
 
 import "fmt"
 
-func removeElement_2(nums []int, val int) int {
+//双指针指针
+func removeElement_doubleIndex(nums []int, val int) int {
 	n := len(nums)
 	left, right := 0, n-1
-	for ; left<n; left++{
-		if nums[left] == val{
-			for ; right>left; right--{
-				if nums[right] != val{
-					break
-				}
-			}
-			if left != right{
-				nums[left], nums[right] = nums[right], nums[left]
-			}
-		}
-		if right == left{
-			break
+	for ; left <= right; left++ {
+		if nums[left] == val {
+			nums[left] = nums[right]
+			left--
+			right--
 		}
 	}
-	return right
+	return left
+}
+
+//快慢指针
+func removeElement_fastAndSlowIndex(nums []int, val int) int {
+	n := len(nums)
+	slow, fast := 0, 0
+	for ; fast < n; fast++ {
+		if val != nums[fast] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
 }
 
 func removeElement_force(nums []int, val int) int {
 	n := len(nums)
-	for i:=0; i<n; i++{
-		if nums[i] == val{
-			for j:=i+1; j<n; j++{
+	for i := 0; i < n; i++ {
+		if nums[i] == val {
+			for j := i + 1; j < n; j++ {
 				nums[j-1] = nums[j]
 			}
 			i--
@@ -37,15 +43,18 @@ func removeElement_force(nums []int, val int) int {
 	return n
 }
 
-func main(){
+func main() {
 	var nums []int
 	nums = []int{2}
-	fmt.Println(removeElement_force(nums, 3))
+	fmt.Println(removeElement_fastAndSlowIndex(nums, 3))
 	fmt.Println(nums)
-	nums = []int{3,2,2,3}
-	fmt.Println(removeElement_force(nums, 3))
+	nums = []int{3, 2, 2, 3}
+	fmt.Println(removeElement_fastAndSlowIndex(nums, 3))
 	fmt.Println(nums)
-	nums = []int{0,1,2,2,3,0,4,2}
-	fmt.Println(removeElement_force(nums, 2))
+	nums = []int{0, 1, 2, 2, 3, 0, 4, 2}
+	fmt.Println(removeElement_fastAndSlowIndex(nums, 2))
+	fmt.Println(nums)
+	nums = []int{0, 1, 2, 2, 3, 0, 4, 1}
+	fmt.Println(removeElement_fastAndSlowIndex(nums, 2))
 	fmt.Println(nums)
 }
