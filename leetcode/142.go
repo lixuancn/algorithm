349390d17940dcfffdffc4fd03f5129fd3be728f
lixuancn <lixuan868686@163.com>
2022-03-09 23:55:34 +0800

    环形链表2

---
+++ leetcode/142.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			for slow != head {
+				slow = slow.Next
+				head = head.Next
+			}
+			return head
+		}
+	}
+	return nil
+}
+
+func main() {
+	//head := detectCycle(newList([]int{1, 2, 3, 4, 5}))
+	head := detectCycle(newList([]int{1}))
+	fmt.Println(head)
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func newList(arr []int) *ListNode {
+	var nextNode *ListNode = nil
+	for i := len(arr) - 1; i >= 0; i-- {
+		node := &ListNode{Val: arr[i], Next: nextNode}
+		nextNode = node
+	}
+	return nextNode
+}
