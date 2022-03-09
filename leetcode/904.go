ccc78e52b0082641177e44893cc44730882197c0
lixuancn <lixuan868686@163.com>
2022-03-09 17:41:26 +0800

    数组

--- leetcode/904.go
+++ leetcode/904.go
@@ -3,33 +3,50 @@ package main
 import "fmt"
 
 //滑动窗口
-func totalFruit(fruits []int) int {
-	record := make(map[int]int)
-	left, right, size := 0, 0, 0
-	for ; right < len(fruits); right++ {
-		record[fruits[right]]++
-		for len(record) >= 3 {
-			record[fruits[left]]--
-			if record[fruits[left]] <= 0 {
-				delete(record, fruits[left])
-			}
-			left++
-		}
-		if size < right-left+1 {
-			size = right - left + 1
-		}
+func generateMatrix(n int) [][]int {
+	matrix := make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, n)
 	}
-	return size
+	left, right := 0, n-1
+	top, bottom := 0, n-1
+	num := 1
+	tar := n * n
+	for num <= tar {
+		for i := left; i <= right; i++ {
+			fmt.Println(top, "-", i)
+			matrix[top][i] = num
+			num++
+		}
+		top++
+		for i := top; i <= bottom; i++ {
+			fmt.Println(top, "-", i)
+			matrix[i][right] = num
+			num++
+		}
+		right--
+		for i := right; i >= left; i-- {
+			matrix[bottom][i] = num
+			num++
+		}
+		bottom--
+		for i := bottom; i >= top; i-- {
+			matrix[i][left] = num
+			num++
+		}
+		left++
+	}
+	return matrix
 }
 
 func main() {
-	var nums []int
-	nums = []int{1, 2, 1}
-	fmt.Println(totalFruit(nums))
-	nums = []int{0, 1, 2, 2}
-	fmt.Println(totalFruit(nums))
-	nums = []int{1, 2, 3, 2, 2}
-	fmt.Println(totalFruit(nums))
-	nums = []int{3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4}
-	fmt.Println(totalFruit(nums))
+	fmt.Println(generateMatrix(3))
+	fmt.Println(generateMatrix(1))
+	return
+	//var nums []int
+	//nums = []int{1, 2, 1}
+	//fmt.Println(generateMatrix(nums))
+	//nums = []int{0, 1, 2, 2}
+	//fmt.Println(generateMatrix(nums))
+	//nums = []int{1, 2, 3, 2, 2}
 }
