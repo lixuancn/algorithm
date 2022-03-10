648faf944b525926458cb03ef1813954fa15ed99
lixuancn <lixuan868686@163.com>
2022-03-10 11:17:46 +0800

    两个数组的交集

--- leetcode/349.go
+++ leetcode/349.go
@@ -4,23 +4,26 @@ import (
 	"fmt"
 )
 
-func isAnagram(s string, t string) bool {
-	record := make(map[int32]int)
-	for _, c := range s {
-		record[c]++
+func intersection(nums1 []int, nums2 []int) []int {
+	record := make(map[int]int)
+	for _, n := range nums1 {
+		record[n] = 1
 	}
-	for _, c := range t {
-		record[c]--
-	}
-	for _, v := range record {
-		if v != 0 {
-			return false
+	for _, n := range nums2 {
+		if _, ok := record[n]; ok {
+			record[n] = 0
 		}
 	}
-	return true
+	ret := make([]int, 0)
+	for k, v := range record {
+		if v == 0 {
+			ret = append(ret, k)
+		}
+	}
+	return ret
 }
 
 func main() {
-	fmt.Println(isAnagram("anagram", "nagaram"))
-	fmt.Println(isAnagram("rat", "car"))
+	fmt.Println(intersection([]int{1, 2, 2, 1}, []int{2, 2}))
+	fmt.Println(intersection([]int{4, 9, 5}, []int{9, 4, 9, 8, 4}))
 }
