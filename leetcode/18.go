5bb0930aa7d28bd69c355c3cf82c4d1a7867867e
lixuancn <lixuan868686@163.com>
2022-03-10 19:39:04 +0800

    四数之和

---
+++ leetcode/18.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func fourSum(nums []int, target int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	res := make([][]int, 0)
+	for i := 0; i < n; i++ {
+		if i >= 1 && nums[i-1] == nums[i] {
+			continue
+		}
+		for j := i + 1; j < n; j++ {
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+			left := j + 1
+			right := n - 1
+			for left < right {
+				if nums[i]+nums[j]+nums[left]+nums[right] > target {
+					right--
+				} else if nums[i]+nums[j]+nums[left]+nums[right] < target {
+					left++
+				} else {
+					res = append(res, []int{nums[i], nums[j], nums[left], nums[right]})
+					for left < right && nums[left] == nums[left+1] {
+						left++
+					}
+					for left < right && nums[right] == nums[right-1] {
+						right--
+					}
+					left++
+					right--
+				}
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+	//fmt.Println(fourSum([]int{1, 0, -1, 0, -2, 2}, 0))
+	fmt.Println(fourSum([]int{2, 2, 2, 2, 2}, 8))
+}
