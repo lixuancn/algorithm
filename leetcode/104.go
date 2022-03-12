0fed0b446b53b200830af0b98a5786ad3451c211
lixuancn <lixuan868686@163.com>
2022-03-12 18:37:09 +0800

    二叉树的最大深度

---
+++ leetcode/104.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"container/list"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	depth := 0
+	if root == nil {
+		return depth
+	}
+	queue := list.New()
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		n := queue.Len()
+		depth++
+		for i := 0; i < n; i++ {
+			node := queue.Remove(queue.Front()).(*Node)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+	}
+	return depth
+}
