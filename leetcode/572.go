ab2b8a98cabd3b6bc40add05f348fb44565535d7
lixuancn <lixuan868686@163.com>
2022-03-16 21:27:06 +0800

    572. 另一棵树的子树

--- leetcode/572.go
+++ leetcode/572.go
@@ -7,5 +7,19 @@ type TreeNode struct {
 }
 
 func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	return check(root, subRoot) || check(root.Left, subRoot) || check(root.Right, subRoot)
+}
 
+func check(p, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p != nil || q != nil {
+		return false
+	} else if p.Val != p.Val {
+		return false
+	}
+	return check(p.Left, q.Left) && check(p.Right, q.Right)
 }
