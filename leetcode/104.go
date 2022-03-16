fc626777a9cee2e22ed9a431e10251400d205a48
lixuancn <lixuan868686@163.com>
2022-03-16 12:52:30 +0800

    104. 二叉树的最大深度 迭代、前序、后序

--- leetcode/104.go
+++ leetcode/104.go
@@ -11,6 +11,13 @@ type TreeNode struct {
 }
 
 func maxDepth(root *TreeNode) int {
+	//return maxDepth_iteration(root)
+	return maxDepth_recursion_1(root)
+	return maxDepth_recursion_2(root)
+}
+
+//迭代 - 层序遍历
+func maxDepth_iteration(root *TreeNode) int {
 	depth := 0
 	if root == nil {
 		return depth
@@ -31,4 +38,52 @@ func maxDepth(root *TreeNode) int {
 		}
 	}
 	return depth
+}
+
+//后序遍历
+func maxDepth_recursion_1(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	d1 := maxDepth_recursion_1(root.Left)
+	d2 := maxDepth_recursion_1(root.Right)
+	return max(d1, d2) + 1
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+//前序遍历
+var result int
+
+func maxDepth_recursion_2(root *TreeNode) int {
+	result = 0
+	if root == nil {
+		return result
+	}
+	front_recursion(root, 1)
+	return result
+}
+
+func front_recursion(root *TreeNode, depth int) {
+	if result < depth {
+		result = depth
+	}
+	if root.Left == nil && root.Right == nil {
+		return
+	}
+	if root.Left != nil {
+		depth++
+		front_recursion(root.Left, depth)
+		depth--
+	}
+	if root.Right != nil {
+		depth++
+		front_recursion(root.Right, depth)
+		depth--
+	}
 }
