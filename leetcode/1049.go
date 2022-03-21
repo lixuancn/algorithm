a92eb27627506e0c3134cc00a834bd7c69845c47
lixuancn <lixuan868686@163.com>
2022-03-21 13:50:16 +0800

    494. 目标和 10491049. 最后一块石头的重量 II

--- leetcode/1049.go
+++ leetcode/1049.go
@@ -5,31 +5,14 @@ import (
 )
 
 func main() {
-	r := lastStoneWeightII([]int{31,26,33,21,40})
-	fmt.Println(r)
+	//fmt.Println(lastStoneWeightII([]int{31, 26, 33, 21, 40}))
+	fmt.Println(lastStoneWeightII([]int{2, 4, 1, 1}))
 }
 
-//动态规划优化
-//由于dp[i][j]只和上一行相关，则可以把第一维去掉
 func lastStoneWeightII(stones []int) int {
-	sum := 0
-	for _, stone := range stones{
-		sum += stone
-	}
-	diff := sum / 2
-	dp := make([]bool, diff+1)
-	dp[0] = true
-	for _, stone := range stones{
-		for j:=diff; j>=stone; j--{
-			dp[j] = dp[j] || dp[j-stone]
-		}
-	}
-	for j:=diff; j>=0; j--{
-		if dp[j]{
-			return sum - 2 * j
-		}
-	}
-	return 0
+	return lastStoneWeightII_dp1(stones)
+	//return lastStoneWeightII_dp2(stones)
+	return lastStoneWeightII_dp3(stones) //动态规划 一维 代码随想录
 }
 
 //动态规划 - 背包问题 和494类似
@@ -37,34 +20,83 @@ func lastStoneWeightII(stones []int) int {
 //sum表示stones所有元素之和，neg表示所有为负的元素和，为正的元素和就是sum-neg
 //粉碎后剩余的石头为(sum-neg)-neg = sum - 2*neg。 剩余石头最小时neg要无限接近于sum/2
 //背包问题：背包容量为sum/2，物品重量和价值均为stones[i]的0-1背包问题。
-//dp[i][j]表示前i个元素，剩余重量是否为j。
+//dp[i][j]表示前i个元素，剩余重量是否为j。(前i个石头能否凑出重量j)
 //边界：i=0时，j=0为真，其余j均为假
 //	j<stones[i]，则不能选，dp[i][j] = dp[i-1][j]
 //  j>=stones[i]，若不选，dp[i][j] = dp[i-1][j]；若选，dp[i][j] = dp[i-1][j-stones[i]]。看这种方法谁为真。
 //遍历dp[n]，最大的j就是答案
-func lastStoneWeightII_dp(stones []int) int {
+func lastStoneWeightII_dp1(stones []int) int {
 	sum, n := 0, len(stones)
-	for _, stone := range stones{
+	for _, stone := range stones {
 		sum += stone
 	}
 	diff := sum / 2
 	dp := make([][]bool, n+1)
-	for i := range dp{
+	for i := range dp {
 		dp[i] = make([]bool, diff+1)
 	}
 	dp[0][0] = true
-	for i, stone := range stones{
-		for j:=0; j<=diff; j++{
+	for i, stone := range stones {
+		for j := 0; j <= diff; j++ {
+			fmt.Println(i, j)
 			dp[i+1][j] = dp[i][j]
-			if j >= stone{
+			if j >= stone {
 				dp[i+1][j] = dp[i+1][j] || dp[i][j-stone]
 			}
+			fmt.Println(dp)
 		}
 	}
-	for j:=diff; j>=0; j--{
-		if dp[n][j]{
-			return sum - 2 * j
+	for j := diff; j >= 0; j-- {
+		if dp[n][j] {
+			return sum - 2*j
 		}
 	}
 	return 0
+}
+
+//动态规划优化
+//由于dp[i][j]只和上一行相关，则可以把第一维去掉
+func lastStoneWeightII_dp2(stones []int) int {
+	sum := 0
+	for _, stone := range stones {
+		sum += stone
+	}
+	diff := sum / 2
+	dp := make([]bool, diff+1)
+	dp[0] = true
+	for _, stone := range stones {
+		for j := diff; j >= stone; j-- {
+			dp[j] = dp[j] || dp[j-stone]
+		}
+	}
+	for j := diff; j >= 0; j-- {
+		if dp[j] {
+			return sum - 2*j
+		}
+	}
+	return 0
+}
+
+//01背包问题，重量和价值都是stones[i]，求哪个组合更接近sum/2，这样碰撞就更小
+//dp[j]表示容量为j的背包所能装入的最大价值
+//如果不装，dp[j] = dp[j]
+//如果装，dp[j] = dp[j-stones[i]]+stones[i]
+//记得j必须倒序
+func lastStoneWeightII_dp3(stones []int) int {
+	sum := 0
+	for _, stone := range stones {
+		sum += stone
+	}
+	target := sum / 2
+	dp := make([]int, target+1)
+	for i := 0; i < len(stones); i++ {
+		for j := target; j >= stones[i]; j-- {
+			//和max(dp[j], dp[j-stones[i]]+stones[i])是一样的
+			if dp[j] < dp[j-stones[i]]+stones[i] {
+				dp[j] = dp[j-stones[i]] + stones[i]
+			}
+		}
+	}
+	//剩余的一推石头价值 - 我们跳出来的石头价值
+	return (sum - dp[target]) - dp[target]
 }
--- leetcode/494.go
+++ leetcode/494.go
@@ -1,29 +1,40 @@
 package main
+
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	r := findTargetSumWays([]int{1000}, -1000)
-	fmt.Println(r)
+	fmt.Println(findTargetSumWays_dp1([]int{1, 1, 1, 1, 1}, 3))
+	fmt.Println(findTargetSumWays_dp2([]int{1, 1, 1, 1, 1}, 3))
+	fmt.Println(findTargetSumWays_dp3([]int{1, 1, 1, 1, 1}, 3)) //代码随想录
+	fmt.Println(findTargetSumWays_recursion([]int{1, 1, 1, 1, 1}, 3))
+}
+
+func findTargetSumWays(nums []int, target int) int {
+	return findTargetSumWays_dp1(nums, target)
+	return findTargetSumWays_dp2(nums, target)
+	return findTargetSumWays_dp3(nums, target)
+	return findTargetSumWays_recursion(nums, target)
 }
 
 //动态规划优化版
 //每次循环只和上一行有关，所以去掉数组第一维。
-func findTargetSumWays(nums []int, target int) int {
+func findTargetSumWays_dp1(nums []int, target int) int {
 	sum := 0
-	for _, num := range nums{
+	for _, num := range nums {
 		sum += num
 	}
 	diff := sum - target
-	if diff < 0 || diff % 2 != 0{
+	if diff < 0 || diff%2 != 0 {
 		return 0
 	}
 	neg := diff / 2
 	dp := make([]int, neg+1)
 	dp[0] = 1
-	for i := range nums{
-		for j:=neg; j>=nums[i]; j--{
+	for i := range nums {
+		for j := neg; j >= nums[i]; j-- {
 			dp[j] += dp[j-nums[i]]
 		}
 	}
@@ -41,24 +52,24 @@ func findTargetSumWays(nums []int, target int) int {
 //1<=i<=n时，遍历0<=j<=neg计算dp[i][j]：
 //	如果j<num，则不能选num，dp[i][j] = dp[i-1][j]
 //	如果j>num，如果不选num，dp[i][j] = dp[i-1][j]，如果选，dp[i][j] = dp[i-1][j]+dp[i-1][j-num]
-func findTargetSumWays_dp(nums []int, target int) int {
+func findTargetSumWays_dp2(nums []int, target int) int {
 	sum, n := 0, len(nums)
-	for _, num := range nums{
+	for _, num := range nums {
 		sum += num
 	}
 	diff := sum - target
-	if diff < 0 || diff % 2 != 0{
+	if diff < 0 || diff%2 != 0 {
 		return 0
 	}
 	neg := diff / 2
 	dp := make([][]int, n+1)
 	dp[0] = make([]int, neg+1)
 	dp[0][0] = 1
-	for i := range nums{
+	for i := range nums {
 		dp[i+1] = make([]int, neg+1)
-		for j:=0; j<=neg; j++{
+		for j := 0; j <= neg; j++ {
 			dp[i+1][j] = dp[i][j]
-			if j >= nums[i]{
+			if j >= nums[i] {
 				dp[i+1][j] += dp[i][j-nums[i]]
 			}
 		}
@@ -66,8 +77,36 @@ func findTargetSumWays_dp(nums []int, target int) int {
 	return dp[n][neg]
 }
 
+//01背包问题，left+right = sum      left-right=target ->  left-(sum-left)=target -> left=(target+sum)/2
+//转化为挑选背包容量为neg的元素，即和为neg的时候有几种选择法
+func findTargetSumWays_dp3(nums []int, target int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	if target > sum {
+		return 0
+	}
+	if int(math.Abs(float64(target))) > sum {
+		return 0
+	}
+	if (target+sum)%2 != 0 {
+		return 0
+	}
+	neg := (sum + target) / 2
+	dp := make([]int, neg+1)
+	dp[0] = 1
+	for i := 0; i < len(nums); i++ {
+		for j := neg; j >= nums[i]; j-- {
+			dp[j] = dp[j] + dp[j-nums[i]]
+		}
+	}
+	return dp[neg]
+}
+
 //递归
 var count int
+
 func findTargetSumWays_recursion(nums []int, target int) int {
 	count = 0
 	recursion(nums, target, len(nums), 0, nums[0])
@@ -75,9 +114,9 @@ func findTargetSumWays_recursion(nums []int, target int) int {
 	return count
 }
 
-func recursion(nums []int, target, n, index, sum int){
-	if index == n - 1{
-		if sum == target{
+func recursion(nums []int, target, n, index, sum int) {
+	if index == n-1 {
+		if sum == target {
 			count++
 		}
 		return
