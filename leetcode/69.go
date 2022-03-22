00cc62788a26e13e63bd59ab4a1c5131281bc3ce
lixuancn <lixuan868686@163.com>
2022-03-22 20:42:39 +0800

    69. x 的平方根

---
+++ leetcode/69.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func mySqrt(x int) int {
+	if x <= 1 {
+		return x
+	}
+	left, right := 0, x
+	ans := 0
+	for left <= right {
+		mid := (right-left)/2 + left
+		if mid*mid <= x {
+			ans = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return ans
+}
+
+func main() {
+	fmt.Println(mySqrt(4))
+	fmt.Println(mySqrt(8))
+}
