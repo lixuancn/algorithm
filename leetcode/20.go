d2bf51b29434a042fea988058ca9b86213ae3781
lixuancn <lixuan868686@163.com>
2022-03-31 00:15:03 +0800

    20. 有效的括号

--- leetcode/20.go
+++ leetcode/20.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"container/list"
+	"fmt"
+)
+
 func isValid(s string) bool {
 	stack := make([]byte, 0)
 	if len(s) == 0 {
@@ -24,4 +29,26 @@ func isValid(s string) bool {
 		return false
 	}
 	return true
+}
+
+func isValid_practice(s string) bool {
+	stack := list.New()
+	for _, c := range s {
+		if c == '(' {
+			stack.PushBack(')')
+		} else if c == '[' {
+			stack.PushBack(']')
+		} else if c == '{' {
+			stack.PushBack('}')
+		} else if stack.Len() == 0 {
+			return false
+		} else if stack.Remove(stack.Back()).(rune) != c {
+			return false
+		}
+	}
+	return stack.Len() == 0
+}
+
+func main() {
+	fmt.Println(isValid_practice("()"))
 }
