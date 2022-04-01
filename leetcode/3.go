1c84e239c8325f2cd0438fccdea319100494f492
lixuancn <lixuan868686@163.com>
2022-04-01 10:48:00 +0800

    3. 无重复字符的最长子串

--- leetcode/3.go
+++ leetcode/3.go
@@ -1,20 +1,18 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func lengthOfLongestSubstring(s string) int {
 	n := len(s)
-	if n == 0{
+	if n == 0 {
 		return 0
 	}
 	cache := make(map[uint8]int)
 	max := 0
 	left, right := 0, 0
-	for right=0; right<n; right++{
+	for right = 0; right < n; right++ {
 		c := s[right]
-		if _, ok := cache[c]; ok{
+		if _, ok := cache[c]; ok {
 			left = maxInt(left, cache[c]+1)
 		}
 		cache[c] = right
@@ -23,15 +21,31 @@ func lengthOfLongestSubstring(s string) int {
 	return max
 }
 
-func maxInt(a, b int)int{
+func maxInt(a, b int) int {
 	if a > b {
 		return a
 	}
 	return b
 }
 
-func main(){
-	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
-	fmt.Println(lengthOfLongestSubstring("bbbbb"))
-	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+func main() {
+	fmt.Println(lengthOfLongestSubstring_practice("abba"))
+	//fmt.Println(lengthOfLongestSubstring_practice("abcabcbb"))
+	//fmt.Println(lengthOfLongestSubstring_practice("bbbbb"))
+	//fmt.Println(lengthOfLongestSubstring_practice("pwwkew"))
+}
+
+func lengthOfLongestSubstring_practice(s string) int {
+	left, right := -1, 0
+	l := 0
+	used := map[uint8]int{}
+	for ; right < len(s); right++ {
+		if _, ok := used[s[right]]; ok {
+			left = maxInt(left, used[s[right]])
+		}
+		used[s[right]] = right
+		fmt.Println(right, left)
+		l = maxInt(l, right-left)
+	}
+	return l
 }
