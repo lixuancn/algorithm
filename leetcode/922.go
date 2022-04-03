8a727b15caaadf9b6e877bfb41e12810bb6a91fc
lixuancn <lixuan868686@163.com>
2022-04-03 13:26:52 +0800

    922. 按奇偶排序数组 II

--- leetcode/922.go
+++ leetcode/922.go
@@ -15,6 +15,23 @@ func sortArrayByParityII(nums []int) []int {
 	return nums
 }
 
+func sortArrayByParityII_practice(nums []int) []int {
+	//i表示偶数，j表示奇数
+	for i, j := 0, 1; i < len(nums) && j < len(nums); i, j = i+2, j+2 {
+		//i找奇数
+		for ; i < len(nums) && nums[i]&1 == 0; i += 2 {
+		}
+		//j找偶数
+		for ; j < len(nums) && nums[j]&1 == 1; j += 2 {
+		}
+		//交换
+		if i < len(nums) && j < len(nums) && nums[i]&1 == 1 && nums[j]&1 == 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
+	return nums
+}
+
 func main() {
-	fmt.Println(sortArrayByParityII([]int{4, 2, 5, 7}))
+	fmt.Println(sortArrayByParityII_practice([]int{4, 2, 5, 7}))
 }
