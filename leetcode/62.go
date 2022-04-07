f8fb386edd4c9351f88d18252679017febe64a1d
lixuancn <lixuan868686@163.com>
2022-04-07 13:35:40 +0800

    62. 不同路径

--- leetcode/62.go
+++ leetcode/62.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+//62. 不同路径
+
 //动态规划
 func uniquePaths(m int, n int) int {
 	dp := make([][]int, m)
@@ -20,6 +22,27 @@ func uniquePaths(m int, n int) int {
 	return dp[m-1][n-1]
 }
 
+func uniquePaths_practice(m int, n int) int {
+	//dp[i][j]表示走到第(i,j)处有多少种路径
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		dp[i][0] = 1
+	}
+	for j := 0; j < n; j++ {
+		dp[0][j] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
+
 func main() {
-	fmt.Println(uniquePaths(3, 7))
+	fmt.Println(uniquePaths_practice(3, 7))
+	fmt.Println(uniquePaths_practice(3, 2))
 }
