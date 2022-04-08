4a4dee3e4279e663c867ce3456e94bde5585b1bb
李轩 <lixuan@lixuandeMacBook-M1-Pro.local>
2022-04-08 18:43:55 +0800

    309. 最佳买卖股票时机含冷冻期

--- leetcode/309.go
+++ leetcode/309.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 //309. 最佳买卖股票时机含冷冻期
+
 //dp[i][j] 表示第i天 j状态下的最大现金
 //状态0：买入或持股中
 //状态1：之前卖出的，且不在冷冻期，保持空仓状态
@@ -50,6 +51,19 @@ func maxProfit_1(prices []int) int {
 		prev3 = cur3
 	}
 	return max(max(prev1, prev2), prev3)
+}
+
+func maxProfit_1_practice(prices []int) int {
+	//dp[i][0] 0持股中，1持现金，2今天刚卖，3冷却期
+	dp := make([][4]int, len(prices))
+	dp[0][0], dp[0][1], dp[0][2], dp[0][3] = -prices[0], 0, 0, 0
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], max(dp[i-1][1]-prices[i], dp[i-1][3]-prices[i]))
+		dp[i][1] = max(dp[i-1][1], dp[i-1][3])
+		dp[i][2] = dp[i-1][0] + prices[i]
+		dp[i][3] = dp[i-1][2]
+	}
+	return max(dp[len(prices)-1][1], max(dp[len(prices)-1][2], dp[len(prices)-1][3]))
 }
 
 func max(i, j int) int {
