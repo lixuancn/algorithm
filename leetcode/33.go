9e82d86b6b0ad745ed8a925a13fb9aa767f0855a
lixuancn <lixuan868686@163.com>
2022-04-10 11:33:53 +0800

    33. 搜索旋转排序数组

--- leetcode/33.go
+++ leetcode/33.go
@@ -36,11 +36,40 @@ func search(nums []int, target int) int {
 	return -1
 }
 
+func search_practice(nums []int, target int) int {
+	n := len(nums)
+	if n == 0 {
+		return -1
+	}
+	left, right := 0, n-1
+	for left <= right && left < n && right >= 0 {
+		mid := (left + right) >> 1
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
 func main() {
-	//fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 0))
-	//fmt.Println(search([]int{1}, 1))    //0
-	//fmt.Println(search([]int{1, 3}, 1)) //0
-	//fmt.Println(search([]int{1, 3}, 3)) //1
-	//fmt.Println(search([]int{3, 1}, 1)) //1
-	fmt.Println(search([]int{1, 3, 5}, 1)) //1
+	fmt.Println(search_practice([]int{4, 5, 6, 7, 0, 1, 2}, 0)) //4
+	fmt.Println(search_practice([]int{1}, 1))                   //0
+	fmt.Println(search_practice([]int{1, 3}, 1))                //0
+	fmt.Println(search_practice([]int{1, 3}, 3))                //1
+	fmt.Println(search_practice([]int{3, 1}, 1))                //1
+	fmt.Println(search_practice([]int{1, 3, 5}, 1))             //0
+	fmt.Println(search_practice([]int{5, 1, 3}, 5))             //0
 }
---
+++ leetcode/面试题10.03.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func search(arr []int, target int) int {
+	n := len(arr)
+	if n == 0 {
+		return -1
+	}
+	left, right := 0, n-1
+	for left <= right {
+		mid := (left + right) >> 1
+		if arr[mid] == target {
+			if mid == 0 {
+				return 0
+			} else if mid > 0 && arr[mid] != arr[mid-1] {
+				return mid
+			}
+		}
+		if arr[mid] < target {
+		}
+	}
+}
+
+func main() {
+	fmt.Println(search([]int{15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}, 5))  //8
+	fmt.Println(search([]int{15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}, 11)) //-1
+	fmt.Println(search([]int{1}, 11))                                         //-1
+}
