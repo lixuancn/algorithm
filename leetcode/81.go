eca5433300985f3c42a761b8665b6e7d8eeb6be7
lixuancn <lixuan868686@163.com>
2022-04-10 11:50:04 +0800

    81. 搜索旋转排序数组 II

--- leetcode/81.go
+++ leetcode/81.go
@@ -11,7 +11,6 @@ func search(nums []int, target int) bool {
 	}
 	left, right := 0, len(nums)-1
 	for left <= right {
-		fmt.Println(left, right)
 		mid := (left + right) / 2
 		if nums[mid] == target {
 			return true
@@ -36,8 +35,40 @@ func search(nums []int, target int) bool {
 	return false
 }
 
+func search_practice(nums []int, target int) bool {
+	n := len(nums)
+	if n == 0 {
+		return false
+	}
+	left, right := 0, n-1
+	for left <= right && left < n && right >= 0 {
+		mid := (left + right) >> 1
+		if target == nums[mid] {
+			return true
+		} else if nums[left] == nums[mid] && nums[mid] == nums[right] {
+			//比如[3，1，2，3，3，3,3],target=2 第一次循环时，无法判断target属于[0,3]还是[4,6]的区间
+			left++
+			right--
+		} else if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return false
+}
+
 func main() {
-	//fmt.Println(search([]int{2, 5, 6, 0, 0, 1, 2}, 0))
-	fmt.Println(search([]int{2, 5, 6, 0, 0, 1, 2}, 3))
-	//fmt.Println(search([]int{1, 0, 1, 1, 1}, 0))
+	//fmt.Println(search_practice([]int{2, 5, 6, 0, 0, 1, 2}, 0)) //true
+	//fmt.Println(search_practice([]int{2, 5, 6, 0, 0, 1, 2}, 3)) //false
+	//fmt.Println(search_practice([]int{1, 0, 1, 1, 1}, 0))       //true
+	fmt.Println(search_practice([]int{3, 1, 2, 3, 3, 3, 3}, 2)) //true
 }
