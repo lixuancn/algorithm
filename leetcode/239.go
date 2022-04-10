51117f387eab757c63fc0bb4f9ed828965115c41
lixuancn <lixuan868686@163.com>
2022-04-10 22:28:05 +0800

    239. 滑动窗口最大值

--- leetcode/239.go
+++ leetcode/239.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+//239. 滑动窗口最大值
+
 //封装一个单调队列
 type MyQueue struct {
 	queue []int
@@ -55,4 +57,54 @@ func maxSlidingWindow(nums []int, k int) []int {
 
 func main() {
 	fmt.Println(maxSlidingWindow([]int{1, 3, -1, -3, 5, 3, 6, 7}, 3))
+	fmt.Println(maxSlidingWindow_practice([]int{1, 3, -1, -3, 5, 3, 6, 7}, 3))
+}
+
+//封装一个单调队列
+type IncrQueue struct {
+	queue []int
+}
+
+func NewIncrQueue() *IncrQueue {
+	return &IncrQueue{}
+}
+
+func (q *IncrQueue) Empty() bool {
+	return len(q.queue) == 0
+}
+
+func (q *IncrQueue) Front() int {
+	return q.queue[0]
+}
+
+func (q *IncrQueue) Back() int {
+	return q.queue[len(q.queue)-1]
+}
+
+func (q *IncrQueue) Push(v int) {
+	for !q.Empty() && q.Back() < v {
+		q.queue = q.queue[:len(q.queue)-1]
+	}
+	q.queue = append(q.queue, v)
+}
+
+func (q *IncrQueue) Pop(v int) {
+	if !q.Empty() && q.Front() == v {
+		q.queue = q.queue[1:]
+	}
+}
+
+func maxSlidingWindow_practice(nums []int, k int) []int {
+	queue := NewIncrQueue()
+	for i := 0; i < k; i++ {
+		queue.Push(nums[i])
+	}
+	result := make([]int, len(nums)-k+1)
+	result[0] = queue.Front()
+	for i := k; i < len(nums); i++ {
+		queue.Pop(nums[i-k])
+		queue.Push(nums[i])
+		result[i-k+1] = queue.Front()
+	}
+	return result
 }
